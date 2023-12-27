5d6ca0229d017cd54914f2bad4c985d459c2cbae
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-12-27 16:11:50 -0500

    Login com google funcionando

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const express = require('express');
-const connectToMongoDb = require('./app/config/db.config');
+const connectToMongoDb = require('./app/config/db.connect');
 const app = express();
+const session = require('express-session');
+const authRouter = require('./app/controllers/google-auth');
+const passport = require('passport');
+
+app.set('view engine', 'ejs');
 
 connectToMongoDb();
 
-app.listen(3000, () => console.log('App running...'));
+app.use(
+session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
+
+app.get('/', (req, res) => {
+res.render('auth');
+});
+
+app.use('/auth/google', authRouter);
+
+app.listen(3000, () => console.log('App is running on port 3000...'));
